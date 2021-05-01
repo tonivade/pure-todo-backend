383616ac08ddeda10cfc2fa8f088cf7ca278341a
Antonio Muñoz <antoniogmc@gmail.com>
2021-05-01 16:43:13 +0200

    minor change

--- src/main/java/com/github/tonivade/todo/domain/TodoRepository.java
+++ src/main/java/com/github/tonivade/todo/domain/TodoRepository.java
@@ -5,13 +5,13 @@
 package com.github.tonivade.todo.domain;
 
 import static com.github.tonivade.purefun.type.Option.none;
+
 import com.github.tonivade.purefun.Kind;
 import com.github.tonivade.purefun.Operator1;
 import com.github.tonivade.purefun.Unit;
 import com.github.tonivade.purefun.Witness;
 import com.github.tonivade.purefun.data.Sequence;
 import com.github.tonivade.purefun.type.Option;
-import com.github.tonivade.purefun.typeclasses.For;
 import com.github.tonivade.purefun.typeclasses.Monad;
 
 public interface TodoRepository<F extends Witness> {
@@ -26,7 +26,7 @@ public interface TodoRepository<F extends Witness> {
   Kind<F, Unit> delete(Id id);
 
   default Kind<F, Option<Todo>> modify(Id id, Operator1<Todo> update) {
-    return For.with(monad())
+    return monad().use()
         .andThen(() -> find(id))
         .map(option -> option.map(update::apply))
         .flatMap(option -> option.<Kind<F, Option<Todo>>>fold(() -> monad().pure(none()), this::update))
