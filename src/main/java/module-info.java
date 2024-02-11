f69ad2816b466d619eb17dc0e03324d4c5300f47
Antonio Muñoz <antoniogmc@gmail.com>
2024-02-11 22:26:44 +0100

    java modules v2

---
+++ src/main/java/module-info.java
@@ -0,0 +1,19 @@
+
+module com.github.tonivade.todo {
+  exports com.github.tonivade.todo;
+
+  requires com.github.tonivade.purecfg;
+  requires com.github.tonivade.puredbc;
+  requires com.github.tonivade.purefun;
+  requires com.github.tonivade.purefun.core;
+  requires com.github.tonivade.purefun.effect;
+  requires com.github.tonivade.purefun.free;
+  requires com.github.tonivade.purefun.transformer;
+  requires com.github.tonivade.purefun.typeclasses;
+  requires com.github.tonivade.purejson;
+  requires com.zaxxer.hikari;
+  requires org.slf4j;
+  requires java.sql;
+  requires com.github.tonivade.zeromock.api;
+  requires com.github.tonivade.zeromock.server;
+}
