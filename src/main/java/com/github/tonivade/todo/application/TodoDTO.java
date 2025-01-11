19fe0432b9dd9ed3cd19baf147abfb3ba03070b4
Antonio Muñoz <antoniogmc@gmail.com>
2025-01-12 00:29:45 +0100

    code cleanup

--- src/main/java/com/github/tonivade/todo/application/TodoDTO.java
+++ src/main/java/com/github/tonivade/todo/application/TodoDTO.java
@@ -10,7 +10,6 @@ import static com.github.tonivade.purefun.type.Validation.requireNonNull;
 import static com.github.tonivade.purefun.type.Validation.requirePositive;
 import static java.util.Objects.nonNull;
 import com.github.tonivade.purefun.type.Either;
-import com.github.tonivade.purefun.type.Validation;
 import com.github.tonivade.purefun.type.Validation.Result;
 import com.github.tonivade.purejson.Json;
 import com.github.tonivade.todo.domain.Todo;
@@ -21,20 +20,23 @@ public record TodoDTO(Integer id, String title, Integer order, Boolean completed
   private static final String BASE_URL = "https://tonivade.es/todo/";
 
   public Either<Throwable, Todo> toDomain() {
-    return Validation.mapN(
+    return mapN(
         requirePositive(id),
         requireNonEmpty(title),
         requirePositive(order),
-        requireNonNull(completed), Todo::create).mapError(Result::join).<Throwable>mapError(IllegalArgumentException::new).toEither();
+        requireNonNull(completed), Todo::create).mapError(Result::join)
+        .<Throwable>mapError(IllegalArgumentException::new).toEither();
   }
 
   public Either<Throwable, Todo> toDraft() {
     if (nonNull(order)) {
       return mapN(
           requireNonEmpty(title),
-          requirePositive(order), Todo::draft).mapError(Result::join).<Throwable>mapError(IllegalArgumentException::new).toEither();
+          requirePositive(order), Todo::draft).mapError(Result::join)
+          .<Throwable>mapError(IllegalArgumentException::new).toEither();
     }
-    return requireNonNull(title).map(Todo::draft).<Throwable>mapError(IllegalArgumentException::new).toEither();
+    return requireNonNull(title).map(Todo::draft)
+        .<Throwable>mapError(IllegalArgumentException::new).toEither();
   }
 
   public static TodoDTO fromDomain(Todo todo) {
